91539eeae3f0d983fde4ef3262e880575353fce9
K3NOX <info@k3nox.de>
2023-03-03 18:11:34 +0000

    march api

--- src/backend/anime_api.ts
+++ src/backend/anime_api.ts
@@ -11,7 +11,7 @@ const AnimeProviders = {
 export type AnimeProvider = keyof typeof AnimeProviders;
 
 export class AnimeApi {
-  host = "https://api.consumet.org";
+  host = "https://march-api.vercel.app";
   provider;
 
   constructor(provider: AnimeProvider = "GOGO") {
