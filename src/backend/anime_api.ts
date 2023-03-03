129f20deeee91ee4f0f523d05993cf365acf0fa2
K3NOX <info@k3nox.de>
2023-03-03 17:05:40 +0000

    new api

--- src/backend/anime_api.ts
+++ src/backend/anime_api.ts
@@ -11,7 +11,7 @@ const AnimeProviders = {
 export type AnimeProvider = keyof typeof AnimeProviders;
 
 export class AnimeApi {
-  host = "https://vercel.com/k3noxofficial/k3noxanime-api/d542aotjNFMZNFFa4mm9wec8s6y8";
+  host = "https://k3noxanime-eqkvt9k25-k3noxofficial.vercel.app/";
   provider;
 
   constructor(provider: AnimeProvider = "GOGO") {
