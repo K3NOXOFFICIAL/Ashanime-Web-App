98067cd3247caa51686f8a933ebfec7127e39c73
K3NOX <info@k3nox.de>
2023-03-08 03:22:00 +0000

    Standard Volume

--- src/components/videoplayer/ArtPlayer.tsx
+++ src/components/videoplayer/ArtPlayer.tsx
@@ -217,7 +217,7 @@ export default function Player({ option, getInstance }: any) {
       autoSize: false,
       autoOrientation: true,
       title: modalData.title.romaji,
-      volume: 0.5,
+      volume: 1.0,
       isLive: false,
       muted: false,
       autoplay: false,
@@ -249,7 +249,7 @@ export default function Player({ option, getInstance }: any) {
           width: 300,
           html: "Subtitles",
           name: "Subs",
-          tooltip: selectedSubtitle ? selectedSubtitle : "German",
+          tooltip: selectedSubtitle ? selectedSubtitle : "English",
           icon: handleSubtitleIcon(),
           selector: subtitles,
           onSelect: function (item: any) {
